2fb5934d446299b5192aafb88eef50ce9f0f6120
Shola Nejo <shola.nejo@zedcrest.com>
2024-07-13 07:09:52 +0100

    added unit tests for user creation

---
+++ ClaySolutionsAutomatedDoor.Test/Members/Commands/AddApplicationUserCommandHandlerTests.cs
@@ -0,0 +1,119 @@
+﻿using ClaySolutionsAutomatedDoor.Application.Common.Models;
+using ClaySolutionsAutomatedDoor.Application.Common.Repositories;
+using ClaySolutionsAutomatedDoor.Application.Features.Admin.Commands;
+using ClaySolutionsAutomatedDoor.Domain.Entities;
+using ClaySolutionsAutomatedDoor.Test.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+
+namespace ClaySolutionsAutomatedDoor.Test.Members.Commands
+{
+    public class AddApplicationUserCommandHandlerTests
+    {
+        private readonly Mock<UserManager<ApplicationUser>> _mockUserManager;
+        private readonly Mock<IUnitOfWorkRepository> _mockUnitOfWorkRepository;
+        private readonly Mock<ILogger> _mockLogger = new();
+        private readonly Mock<IDoorAccessControlGroupRepository> _mockDoorAccessGroup = new();
+        private readonly Mock<IAuditTrailRepository> _mockAuditTrail = new();
+
+        public AddApplicationUserCommandHandlerTests()
+        {
+            _mockUserManager = MockHelpers.MockUserManager(TestData.TestUsers);
+            _mockUnitOfWorkRepository = new Mock<IUnitOfWorkRepository>();
+            _mockUnitOfWorkRepository.Setup(x => x.DoorAccessControlGroupRepository).Returns(_mockDoorAccessGroup.Object);
+            _mockUnitOfWorkRepository.Setup(x => x.AuditTrailRepository).Returns(_mockAuditTrail.Object);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnFailedResponse_WhenEmailIsNotUnique()
+        {
+            //arrange
+            var command = new AddApplicationUserCommand
+            {
+                DoorAccessControlGroupId = TestData.OfficeManager.DoorAccessControlGroupId,
+                FirstName = TestData.OfficeManager.FirstName,
+                CreatedBy = TestData.OfficeManager.CreatedBy,
+                Email = TestData.OfficeManager.Email,
+                LastName = TestData.OfficeManager.LastName,
+                Password = "Password1$",
+            };
+
+            _mockUserManager
+                .Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+                .ReturnsAsync(TestData.OfficeManager);
+            var handler = new AddApplicationUserCommandHandler(_mockUserManager.Object, _mockUnitOfWorkRepository.Object, _mockLogger.Object);
+
+            //act
+            var result = await handler.Handle(command, default);
+
+            //assert
+            result.Status.ShouldBe(false);
+            result.Message.ShouldBe(Constants.UserAlreadyExistsMessage);
+            result.StatusCode.ShouldBe((int)StatusCodes.Status409Conflict);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnFailedResponse_WhenDoorAccessControlGroupIdIsNotFound()
+        {
+            //arrange
+            var command = new AddApplicationUserCommand
+            {
+                DoorAccessControlGroupId = Guid.NewGuid(),
+                FirstName = TestData.OfficeManager.FirstName,
+                CreatedBy = TestData.OfficeManager.CreatedBy,
+                Email = TestData.OfficeManager.Email,
+                LastName = TestData.OfficeManager.LastName,
+                Password = "Password1$",
+            };
+
+            DoorAccessControlGroup controlGroup = null!;
+
+            _mockUnitOfWorkRepository
+                .Setup(x => x.DoorAccessControlGroupRepository
+                .GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(controlGroup);
+            var handler = new AddApplicationUserCommandHandler(_mockUserManager.Object, _mockUnitOfWorkRepository.Object, _mockLogger.Object);
+
+            //act
+            var result = await handler.Handle(command, default);
+
+            //assert
+            result.Status.ShouldBe(false);
+            result.Message.ShouldBe(Constants.DoorAccessGroupNotFoundMessage);
+            result.StatusCode.ShouldBe((int)StatusCodes.Status400BadRequest);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnSuccessResponse_WhenEmailIsUnique()
+        {
+            //arrange
+            var command = new AddApplicationUserCommand
+            {
+                DoorAccessControlGroupId = TestData.OfficeManager.DoorAccessControlGroupId,
+                FirstName = "UniqueFirstName",
+                CreatedBy = TestData.OfficeManager.CreatedBy,
+                Email = "uniqueemailaddress@outlook.com",
+                LastName = "UniqueLastName",
+                Password = "Password1$",
+            };
+
+            _mockUnitOfWorkRepository
+                .Setup(x => x.DoorAccessControlGroupRepository
+                .GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(TestData.RestrictedDoorAccessControlGroup);
+
+            var handler = new AddApplicationUserCommandHandler(_mockUserManager.Object, _mockUnitOfWorkRepository.Object, _mockLogger.Object);
+
+            //act
+            var result = await handler.Handle(command, default);
+
+            //assert
+            result.Status.ShouldBe(true);
+            result.Message.ShouldBe(Constants.Api200OkMessage);
+            result.StatusCode.ShouldBe((int)StatusCodes.Status201Created);
+        }
+    }
+}
---
+++ ClaySolutionsAutomatedDoor.Test/Shared/TestData.cs
@@ -0,0 +1,98 @@
+﻿using ClaySolutionsAutomatedDoor.Domain.Entities;
+
+namespace ClaySolutionsAutomatedDoor.Test.Shared
+{
+    internal class TestData
+    {
+        private const string Password = "StrongPassword1$";
+        private static readonly Guid GeneralDoorAccessControlGroupId = Guid.Parse("1E4993B3-8B6C-40FC-8C68-B31FD3E8C5A4");
+        private static readonly Guid RestrictedDoorAccessControlGroupId = Guid.Parse("5CB3E7C8-FBDA-4D86-AB2D-1111A8D0E4C0");
+
+        #region Users
+
+        public static ApplicationUser OfficeManager = new()
+        {
+            Id = "E586519F-A3D5-4283-BCE5-2292794B4C13",
+            FirstName = "Shola",
+            LastName = "Nejo",
+            Email = "sholanejo@live.com",
+            UserName = "sholanejo@live.com",
+            EmailConfirmed = true,
+            PhoneNumberConfirmed = true,
+            IsActive = true,
+            DoorAccessControlGroupId = RestrictedDoorAccessControlGroupId,
+            CreatedBy = "E586519F-A3D5-4283-BCE5-2292794B4C13",
+            CreatedDate = DateTime.Now
+        };
+
+        static ApplicationUser Director = new()
+        {
+            FirstName = "Timothy",
+            LastName = "Johnson",
+            Email = "timothyjohnson@outlook.com",
+            UserName = "timothyjohnson@outlook.com",
+            EmailConfirmed = true,
+            PhoneNumberConfirmed = true,
+            IsActive = true,
+            DoorAccessControlGroupId = GeneralDoorAccessControlGroupId,
+            CreatedBy = "E586519F-A3D5-4283-BCE5-2292794B4C13",
+            CreatedDate = DateTime.Now
+        };
+
+        static ApplicationUser Employee = new()
+        {
+            FirstName = "Jane",
+            LastName = "Doe",
+            Email = "jane.doe@gmail.com",
+            UserName = "jane.doe@gmail.com",
+            EmailConfirmed = true,
+            PhoneNumberConfirmed = true,
+            IsActive = true,
+            DoorAccessControlGroupId = GeneralDoorAccessControlGroupId,
+            CreatedBy = "E586519F-A3D5-4283-BCE5-2292794B4C13",
+            CreatedDate = DateTime.Now
+        };
+
+        static ApplicationUser InActiveUser = new()
+        {
+            FirstName = "Ada",
+            LastName = "Lovelace",
+            Email = "ada.lovelace@hotmail.com",
+            UserName = "ada.lovelace@outlook.com",
+            EmailConfirmed = true,
+            PhoneNumberConfirmed = true,
+            IsActive = false,
+            DoorAccessControlGroupId = GeneralDoorAccessControlGroupId,
+            CreatedBy = "E586519F-A3D5-4283-BCE5-2292794B4C13",
+            CreatedDate = DateTime.Now
+        };
+        #endregion
+
+        #region Door Access Control Groups
+
+        public static DoorAccessControlGroup RestrictedDoorAccessControlGroup = new()
+        {
+            Id = RestrictedDoorAccessControlGroupId,
+            GroupName = "Restricted Group",
+            DateCreated = DateTime.Now,
+            CreatedBy = "E586519F-A3D5-4283-BCE5-2292794B4C13",
+        };
+
+        public static DoorAccessControlGroup GeneralDoorAccessControlGroup = new()
+        {
+            Id = GeneralDoorAccessControlGroupId,
+            GroupName = "General Group",
+            DateCreated = DateTime.Now,
+            CreatedBy = "E586519F-A3D5-4283-BCE5-2292794B4C13",
+        };
+        #endregion
+
+        public static List<ApplicationUser> TestUsers = new()
+        {
+            InActiveUser,
+            Employee,
+            OfficeManager,
+            Director
+        };
+    }
+}
